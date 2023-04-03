cf315624beaa1ec50513721d9be4869d2816b50f
Adeniyi Olanrewaju <adeniyiboladale@yahoo.com>
2023-04-03 19:45:27 +0100

    auth_user_model set

--- redmark_project/redmark_project/settings.py
+++ redmark_project/redmark_project/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "rest_framework",
     "redmark_app",
 ]
 
@@ -81,6 +82,7 @@ DATABASES = {
     }
 }
 
+AUTH_USER_MODEL = "redmark_app.UserProfile"
 
 # Password validation
 # https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators
