bf29dec8e5bc4f3ea172171cf092919bb2d40d90
Adeniyi Olanrewaju <adeniyiboladale@yahoo.com>
2023-04-05 14:07:27 +0100

    get and post questions

--- redmark_project/redmark_app/endpoints/questions.py
+++ redmark_project/redmark_app/endpoints/questions.py
@@ -3,14 +3,27 @@ from django.http import HttpResponse
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from ..models import Question
+from ..serializers import QuestionSerializer
 
 
 class IndexView(APIView):
     def get(self, request, *args, **kwargs):
         return Response("Hello, world. You're at the polls index.")
 
+
 class QuestionView(APIView):
+
     def get(self, request, *args, **kwargs):
         qs = Question.objects.all()
-        question = qs.first()
-        return Response(question)
+        # question = qs.first()
+        serializer = QuestionSerializer(qs, many=True)
+        # if not question:
+        #     return Response("No questions found")
+        return Response(serializer.data)
+
+    def post(self, request, *args, **kwargs):
+        serializer = QuestionSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors)
