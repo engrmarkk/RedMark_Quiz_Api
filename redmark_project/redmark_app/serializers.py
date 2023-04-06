f4c41a539d9f784bcd03f9019cfd697bb4b35075
Adeniyi Olanrewaju <adeniyiboladale@yahoo.com>
2023-04-06 02:51:37 +0100

    modified the serializers to display the options and answer the question(s)

--- redmark_project/redmark_app/serializers.py
+++ redmark_project/redmark_app/serializers.py
@@ -2,7 +2,22 @@ from rest_framework import serializers
 from .models import *
 
 
+class AnswerSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Answer
+        fields = ('answer',)
+
+
+class OptionsSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Options
+        fields = ('id', 'a', 'b', 'c', 'd', 'e')
+
+
 class QuestionSerializer(serializers.ModelSerializer):
+    answer = AnswerSerializer(read_only=True, source='answer.answer')
+    options = OptionsSerializer(many=True, read_only=True)
+
     class Meta:
         model = Question
-        fields = ("id", "question_text",)
+        fields = ("id", "question_text", "answer", "options")
