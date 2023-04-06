25b8fb0ff5172af68c2d524f6bb28e164570c357
Adeniyi Olanrewaju <adeniyiboladale@yahoo.com>
2023-04-06 16:09:22 +0100

    updated the serializer to display the options alng with the question

--- redmark_project/redmark_app/serializers.py
+++ redmark_project/redmark_app/serializers.py
@@ -15,9 +15,29 @@ class OptionsSerializer(serializers.ModelSerializer):
 
 
 class QuestionSerializer(serializers.ModelSerializer):
-    answer = AnswerSerializer(read_only=True, source='answer.answer')
-    options = OptionsSerializer(many=True, read_only=True)
+    answer = AnswerSerializer(read_only=True, source='question_answers.answer')
+    options = serializers.SerializerMethodField()
 
     class Meta:
         model = Question
         fields = ("id", "question_text", "answer", "options")
+
+    def get_options(self, obj):
+        options = Options.objects.filter(question=obj).first()
+        if not options:
+            return {}
+        return {
+            'a': options.a,
+            'b': options.b,
+            'c': options.c,
+            'd': options.d,
+            'e': options.e
+        }
+
+
+class OptionsSerializerQuestionID(serializers.ModelSerializer):
+    # question_id = serializers.ReadOnlyField(source='question.id')
+
+    class Meta:
+        model = Options
+        fields = ('id', 'question_id', 'a', 'b', 'c', 'd', 'e')
