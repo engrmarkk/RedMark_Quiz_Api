7ffcfb585928b64adadd34a75257ddf29e547492
Adeniyi Olanrewaju <adeniyiboladale@yahoo.com>
2023-04-06 16:10:34 +0100

    updated the urls

--- redmark_project/redmark_app/urls.py
+++ redmark_project/redmark_app/urls.py
@@ -1,9 +1,12 @@
 from django.urls import path
 # from .views import IndexView
 from .endpoints.questions import *
+from .endpoints.options import *
 
 
 urlpatterns = [
     path("", QuestionView.as_view(), name="index"),
     path("<int:pk>/", EachQuestionView.as_view(), name="each_question"),
+    path("options/", OptionView.as_view(), name="options"),
+    path("options/<int:pk>/", EachOptionView.as_view(), name="each_option"),
 ]
--- redmark_project/redmark_project/urls.py
+++ redmark_project/redmark_project/urls.py
@@ -20,4 +20,6 @@ urlpatterns = [
     # path("admin/", admin.site.urls),
     path("", include("redmark_app.urls")),
     path("/<int:pk>/", include("redmark_app.urls")),
+    path("options/", include("redmark_app.urls")),
+    path("options/<int:pk>/", include("redmark_app.urls")),
 ]
