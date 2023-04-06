960f1f07cee4c39c9e4345ec2d03bef1955602dd
Adeniyi Olanrewaju <adeniyiboladale@yahoo.com>
2023-04-06 21:45:03 +0100

    registered the new urls

--- redmark_project/redmark_app/urls.py
+++ redmark_project/redmark_app/urls.py
@@ -1,12 +1,15 @@
 from django.urls import path
-# from .views import IndexView
 from .endpoints.questions import *
 from .endpoints.options import *
+from .endpoints.answers import *
 
 
 urlpatterns = [
     path("", QuestionView.as_view(), name="index"),
     path("<int:pk>/", EachQuestionView.as_view(), name="each_question"),
     path("options/", OptionView.as_view(), name="options"),
-    path("options/<int:pk>/", EachOptionView.as_view(), name="each_option"),
+    path("options/<int:pk>/", PostOptionView.as_view(), name="each_option"),
+    path("answers/", AnswerView.as_view(), name="answer"),
+    path("answers/<int:pk>/", PostAnswerView.as_view(), name="post answer"),
+    path("each-option/<int:pk>/", EachOptionView.as_view(), name="each option")
 ]
