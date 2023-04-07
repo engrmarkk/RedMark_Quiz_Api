38b629a6d40c5ad3ba0369cf512d20214cfd5e60
Adeniyi Olanrewaju <adeniyiboladale@yahoo.com>
2023-04-07 01:32:58 +0100

    regiser the url for the each answer endpoint

--- redmark_project/redmark_app/urls.py
+++ redmark_project/redmark_app/urls.py
@@ -5,11 +5,12 @@ from .endpoints.answers import *
 
 
 urlpatterns = [
-    path("", QuestionView.as_view(), name="index"),
+    path("", QuestionView.as_view(), name="question"),
     path("<int:pk>/", EachQuestionView.as_view(), name="each_question"),
     path("options/", OptionView.as_view(), name="options"),
     path("options/<int:pk>/", PostOptionView.as_view(), name="each_option"),
     path("answers/", AnswerView.as_view(), name="answer"),
     path("answers/<int:pk>/", PostAnswerView.as_view(), name="post answer"),
-    path("each-option/<int:pk>/", EachOptionView.as_view(), name="each option")
+    path("each-option/<int:pk>/", EachOptionView.as_view(), name="each option"),
+    path("each-answer/<int:pk>/", EachAnswerView.as_view(), name="each answer")
 ]
