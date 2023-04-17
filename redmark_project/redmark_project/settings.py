a818469c18e95174aa4d991c6af6ff6235ff00b5
Adeniyi Olanrewaju <adeniyiboladale@yahoo.com>
2023-04-17 23:48:22 +0100

    modified the setting for the auth token

--- redmark_project/redmark_project/settings.py
+++ redmark_project/redmark_project/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "rest_framework",
+    'rest_framework.authtoken',
     "redmark_app",
 ]
 
@@ -124,3 +125,15 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.TokenAuthentication',
+        # other authentication classes go here, if needed
+    ],
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.IsAuthenticated',
+        # other permission classes go here, if needed
+    ],
+}
