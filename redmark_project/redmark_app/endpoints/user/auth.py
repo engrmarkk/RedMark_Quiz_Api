dd77ad939c91d46d4fd8d35db421948b6aa58831
Adeniyi Olanrewaju <adeniyiboladale@yahoo.com>
2023-04-17 20:00:46 +0100

    the login endpoint

--- redmark_project/redmark_app/endpoints/user/auth.py
+++ redmark_project/redmark_app/endpoints/user/auth.py
@@ -1,6 +1,7 @@
-from ...models import UserProfile
+from ...models import User, UserProfile
 from ...serializers import UserSerializer
 from rest_framework.views import APIView
+from rest_framework.authtoken.views import ObtainAuthToken
 from rest_framework.response import Response
 from rest_framework import status
 
@@ -8,10 +9,18 @@ from rest_framework import status
 class RegisterView(APIView):
     def post(self, request):
         serializer = UserSerializer(data=request.data)
+
         if serializer.is_valid():
-            existing_user = UserProfile.objects.filter(email=serializer.validated_data["email"])
+            existing_user = User.objects.filter(email=serializer.validated_data["email"].lower())
             if existing_user:
                 return Response({"message": "User with email already exists"}, status=status.HTTP_400_BAD_REQUEST)
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+class LoginView(ObtainAuthToken):
+    def post(self, request, *args, **kwargs):
+        request.data['email'] = request.data['email'].lower()
+        serializer = self.serializer_class(data=request.data, context={'request': request})
+        serializer.is_valid(raise_exception=True)
