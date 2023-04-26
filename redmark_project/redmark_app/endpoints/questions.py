e51d8fc73a0deef2b02be951e961b503891ebb90
Adeniyi Olanrewaju <adeniyiboladale@yahoo.com>
2023-04-26 23:39:21 +0100

    added the status code to the create question method view

--- redmark_project/redmark_app/endpoints/questions.py
+++ redmark_project/redmark_app/endpoints/questions.py
@@ -1,6 +1,7 @@
 from django.shortcuts import get_object_or_404
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from rest_framework import status
 from ..models import Question
 from ..serializers import QuestionSerializer
 
@@ -15,7 +16,7 @@ class QuestionView(APIView):
         serializer = QuestionSerializer(data=request.data)
         if serializer.is_valid():
             serializer.save()
-            return Response(serializer.data)
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors)
 
 
