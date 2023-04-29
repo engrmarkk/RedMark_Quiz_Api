d9d47f1fe2a5a5d6ec10d4689f53695855ff42a7
Adeniyi Olanrewaju <adeniyiboladale@yahoo.com>
2023-04-29 11:47:12 +0100

    added only quiz as the project included url

--- redmark_project/redmark_project/urls.py
+++ redmark_project/redmark_project/urls.py
@@ -3,16 +3,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("", include("redmark_app.urls")),
-    path("/<int:pk>/", include("redmark_app.urls")),
-    path("options/", include("redmark_app.urls")),
-    path("options/<int:pk>/", include("redmark_app.urls")),
-    path("answers/", include("redmark_app.urls")),
-    path("answers/<int:pk>/", include("redmark_app.urls")),
-    path("each-option/<int:pk>/", include("redmark_app.urls")),
-    path("all_users/", include("redmark_app.urls")),
-    path("register/", include("redmark_app.urls")),
-    path("login/", include("redmark_app.urls")),
-    path("logout/", include("redmark_app.urls")),
-    path("user/<int:pk>/", include("redmark_app.urls")),
+    path("quiz/", include("redmark_app.urls"))
 ]
